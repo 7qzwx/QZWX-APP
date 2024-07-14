8e9b6db12d8e044a8160d5434d54098e5a265cd0
七种文学 <13852787+seven-types-of-literature@user.noreply.gitee.com>
2024-07-15 00:21:09 +0800

    添加XUI库，已经成功创建一个实例！

--- app/src/main/java/com/qzwx/APP/ui/AppIn.kt
+++ app/src/main/java/com/qzwx/APP/ui/AppIn.kt
@@ -1,18 +1,35 @@
 package com.qzwx.APP.ui
 
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
+import android.content.Intent
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
+import com.qzwx.APP.ALLAPPS
 
-// 全部应用页面入口
-    @Composable
-    fun All_apps(navController: NavHostController) {
-        Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+@Composable
+fun All_apps(navController: NavHostController) {
+    val context = LocalContext.current
 
-
-
-
-        }}
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        Button(
+            onClick = {
+                val intent = Intent(context, ALLAPPS::class.java)
+                context.startActivity(intent)
+            },
+            modifier = Modifier
+                .padding(16.dp)
+                .fillMaxWidth()
+        ) {
+            Text(text = "点我进入")
+        }
+    }
+}
